6728c9fe5ec7ca8b5e30eea98108589e7ed08775
DenyingTheTruth <anton.bachikin@gmail.com>
2018-09-21 11:14:57 +0300

    function getLoveTrianglesCount

--- src/index.js
+++ src/index.js
@@ -1,7 +1,18 @@
-/**
- * @param preferences - an array of integers. Indices of people, whom they love
- * @returns number of love triangles
- */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  var i;
+  var j=0;
+  var buf=[];
+  var k = 0;
+
+  for(i=1;i<=preferences.length;i++){
+    if((preferences[preferences[preferences[i-1]-1]-1]===i) && !(buf.indexOf(i-1)!=-1) && (preferences[i-1]!=i)){
+      k+=1;
+      buf[j]=i-1;
+      buf[j+1]=preferences[i-1]-1;
+      buf[j+2]=preferences[preferences[i-1]-1]-1;
+      j+=3;
+    }  
+  }
+  
+  return k;
 };
